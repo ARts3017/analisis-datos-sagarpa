5e39d4c6069ac60c14ce89b63addb9c846e4445d
ARts3017 <36316564+ARts3017@users.noreply.github.com>
2022-10-12 23:09:40 -0500

    se crea el molde para la base de datos

--- producción_agricola_1980.sql
+++ producción_agricola_1980.sql
@@ -1,1 +1,23 @@
 
+create database produccion_agricola
+( 
+  id bigint not null primary key auto_increment, 
+ Anio int(4),
+ Idestado int(2),
+ Nomestado varchar(60),
+ Idciclo int(1),
+ Nomcicloproductivo varchar(40),
+ Idmodalidad int(1),
+ Nommodalidad varchar(40),
+ Idunidadmedida int(1),
+ Nomunidad varchar(40),
+ Idcultivo int(4),
+ Nomcultivo varchar(40),
+ Sembrada long,
+ Cosechada long ,
+ Siniestrada long ,
+ Volumenproduccion long ,
+ Rendimiento long ,
+ Precio float ,
+ Valorproduccion long
+); 
